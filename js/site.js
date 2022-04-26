3698d23c89467a8ab61956ebdd1440d1dd15b5b5
P4L4DIN-BL4CKCOD3 <80567073+P4L4DIN-BL4CKCOD3@users.noreply.github.com>
2022-04-26 06:44:04 +0200

    vscode debugger
    tested app with vscode debugger.

--- js/site.js
+++ js/site.js
@@ -1,36 +1,71 @@
-//get the values from the page
-//start or controller function
+//We start by getting the values from the page
+//To acheive this we need to a start or controller function
+
 function getValues(){
-    //get values from the page
+    
+    //To get the values from the page
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
-    //parse into integers
+    
+    //We need to validate our input and parse them into integers
     startValue = parseInt(startValue);
     endValue = parseInt(endValue);
-    //
+    
     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-        //we call generateNumbers
+        
+        //Here we call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
+        
+        //Here we call displayNumbers
+        displayNumbers(numbers);
+
     } else {
         alert("You must enter integers");
-    }
-    //we call displayNumbers
+    }   
 }
 
-//generate numbers from startvalue to the endvalue
+//Here we need to generate numbers from startvalue to the endvalue
 //logic function(s)
-function generateNumbers(sValue, eValue){
+
+function generateNumbers(sValue, eValue) {
+    
     let numbers = [];
-    //we want to get all numbers from start to end
-    for(let index = sValue; index <= eValue; index ++){
-        //this will execute in a loop until index = eValue
+    
+    //We want to get all the numbers from start to end
+    
+    for(let index = sValue; index <= eValue; index ++) {
+        
+        //This will execute in a loop until index = eValue
         numbers.push(index);
     }
+
     return numbers;
+
 }
 
-//display the numbers and mark even numbers bold
-//display or view function(s)
-function displayNumbers(){
+//Here we want to display the numbers and mark the even numbers in bold
+//Display or view function(s)
+function displayNumbers(numbers){
+    
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        
+        let className = "even";    
+        
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+                className = "even";
+        }
+        else{
+                className = "odd";
+            }
+        
+        templateRows += `<tr><td class="${className}" >${number}</td></tr>`
+    
+    } 
+    
+    document.getElementById("results").innerHTML = templateRows;
 
 }
